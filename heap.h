27aa588bab3085b0883dbf3049516f62ad6ca26d
knoman7714-commits <knoman7714@sdsu.edu>
2025-10-23 14:50:24 -0700

    started the heap.h file logic isnt working need to fix up heap and down heap implementation

--- heap.h
+++ heap.h
@@ -15,22 +15,50 @@ struct MinHeap {
     MinHeap() { size = 0; }
 
     void push(int idx, int weightArr[]) {
+     if (size >64){}
         data [size++] = idx;
         upheap(size, weightArr);
     }
 
     int pop(int weightArr[]) {
-        // TODO: remove and return smallest index
-        // Replace root with last element, then call downheap()
-        return -1; // placeholder
+    if (size == 0) return;
+        int ret = data[0];
+        size--;
+        data[0] = data[size];
+        downheap(0, weightArr);
+        return ret; // placeholder
     }
 
     void upheap(int pos, int weightArr[]) {
-        // TODO: swap child upward while smaller than parent
+        while (pos > 0) {
+            int parent = (pos - 1) / 2;
+            if (data[pos] < data[parent]) {
+                int t = data[parent];
+                data[pos] = data[parent];
+                data[parent] = t;
+                pos = parent;
+            } else break;
+        }
     }
 
     void downheap(int pos, int weightArr[]) {
-        // TODO: swap parent downward while larger than any child
+        while (true) {
+            int left = 2 * pos + 1;
+            int right = 2 * pos;
+            if (left >=size)break;
+            int smallest = data[left];
+            if (right < size && data[right] < smallest) {
+                smallest = data[right];
+            }
+            if (weightArr[data [smallest]]>weightArr[data[pos]]) {
+                int t = data[pos];
+                data[pos] = data[smallest];
+                data[smallest] = t;
+                pos = smallest;
+            }else {
+                break;
+            }
+        }
     }
 };
 
