4e4cb5e1011c428f01ac74d40abb6d80ddbb3d3b
knoman7714-commits <knoman7714@sdsu.edu>
2025-10-25 13:21:35 -0700

    rewore heap.h to the correct logic  it should now build heaps and orders correctly

--- heap.h
+++ heap.h
@@ -11,29 +11,32 @@ using namespace std;
 struct MinHeap {
     int data[64];
     int size;
+    MinHeap():size(0){}
 
-    MinHeap() { size = 0; }
 
     void push(int idx, int weightArr[]) {
-     if (size >64){}
-        data [size++] = idx;
+     if (size >=64)return;
+        data [size] = idx;
         upheap(size, weightArr);
+        size++;
     }
 
     int pop(int weightArr[]) {
-    if (size == 0) return;
+    if (size == 0) return-1;
         int ret = data[0];
         size--;
-        data[0] = data[size];
-        downheap(0, weightArr);
+        if (size > 0) {
+         data[0] = data[size];
+            downheap(0, weightArr);
+        }
         return ret; // placeholder
     }
 
     void upheap(int pos, int weightArr[]) {
         while (pos > 0) {
             int parent = (pos - 1) / 2;
-            if (data[pos] < data[parent]) {
-                int t = data[parent];
+            if (weightArr[data[pos]] < weightArr[data[parent]]) {
+                int t = data[pos];
                 data[pos] = data[parent];
                 data[parent] = t;
                 pos = parent;
@@ -43,21 +46,19 @@ struct MinHeap {
 
     void downheap(int pos, int weightArr[]) {
         while (true) {
-            int left = 2 * pos + 1;
-            int right = 2 * pos;
-            if (left >=size)break;
-            int smallest = data[left];
-            if (right < size && data[right] < smallest) {
-                smallest = data[right];
-            }
-            if (weightArr[data [smallest]]>weightArr[data[pos]]) {
+            int left = 2 * pos + 1, right =2* pos + 2;
+            if (left>=size)break;
+            int smallest = left;
+            if (right < size && weightArr[data [right]] < weightArr [data[left]])
+                smallest = right;
+
+            if (weightArr[data [smallest]]<weightArr[data[pos]]) {
                 int t = data[pos];
                 data[pos] = data[smallest];
                 data[smallest] = t;
                 pos = smallest;
-            }else {
-                break;
-            }
+
+            } else break;
         }
     }
 };
